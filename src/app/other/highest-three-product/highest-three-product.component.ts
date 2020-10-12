06500aa816212fa1c13bee77e29d56f594a5a680
simennielsen <simen.nielsen96@gmail.com>
2020-10-12 19:02:16 +0200

    change foreach to for loop starting index 1

--- src/app/other/highest-three-product/highest-three-product.component.ts
+++ src/app/other/highest-three-product/highest-three-product.component.ts
@@ -10,7 +10,7 @@ export class HighestThreeProductComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.highestThreeProductFromArray([-5, -5, 2, 1, 1, 4]));
+    console.log(this.highestThreeProductFromArray([-10, 9, 5, -6, -10]));
     // Output: 100
   }
 
@@ -25,29 +25,30 @@ export class HighestThreeProductComponent implements OnInit {
     let lowestProductOf2  = numArray[0] * numArray[1]; // lowest product of the 2 first numbers, this will update as we loop through the array
     let highestProductOf3 = numArray[0] * numArray[1] * numArray[2]; // product of the 3 first numbers, this will update as we loop through the array
     //loop through every number of element
-    numArray.forEach((currentNum:number) => {
+    for(let i = 1; i < numArray.length; i++){
       /*  
       replace highestProductOf3 if current number 
       along with the largest product of 2 numbers is greater. 
       Since we have negative numbers we also need to check the lowestProductOf2
       */
-      highestProductOf3 = Math.max(highestProductOf3, currentNum*highestProductOf2, currentNum*lowestProductOf2); 
+     //console.log(currentNum, highestProductOf2, lowestProductOf2);
+      highestProductOf3 = Math.max(highestProductOf3, numArray[i]*highestProductOf2, numArray[i]*lowestProductOf2);
       /*  
       replace highestProductOf2 if current number 
       along with the largest number is greater. 
       Since we have negative numbers we also need to check the lowest number
       */
-      highestProductOf2 = Math.max(highestProductOf2, currentNum*highest, currentNum*lowest);
+      highestProductOf2 = Math.max(highestProductOf2, numArray[i]*highest, numArray[i]*lowest);
       /*  
       replace lowestProductOf2 if current number 
       along with the largest number is smaller. 
       Since we have negative numbers we also need to check the highest number
       */
-      lowestProductOf2 = Math.min(lowestProductOf2, currentNum*highest, currentNum*lowest);
-
-      highest = Math.max(highest, currentNum);// replace highest number if current number is higher
-      lowest = Math.min(lowest, currentNum); // replace lowest number if current number is lower
-    });
+      lowestProductOf2 = Math.min(lowestProductOf2, numArray[i]*highest, numArray[i]*lowest);
+    
+      highest = Math.max(highest, numArray[i]);// replace highest number if current number is higher
+      lowest = Math.min(lowest, numArray[i]); // replace lowest number if current number is lower
+    }
     return highestProductOf3;
   }
 }
